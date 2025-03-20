f478497a6c59403a06b08f4a03ba5a7540fdd86c
ajay-secret <streamsuntail@gmail.com>
2025-03-20 22:26:30 +0530

    Update script.js

--- book/script.js
+++ book/script.js
@@ -1,31 +1,42 @@
 let currentPage = 1;
+let touchStartX = 0;
+let touchEndX = 0;
 
 function toggleClass(e, toggleClassName) {
-  if(e.className.includes(toggleClassName)) {
-    e.className = e.className.replace(' ' + toggleClassName, '');
+  if (e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(" " + toggleClassName, "");
   } else {
-    e.className += ' ' + toggleClassName;
+    e.className += " " + toggleClassName;
   }
 }
 
 function movePage(e, page) {
   if (page == currentPage) {
-    currentPage+=2;
+    currentPage += 2;
     toggleClass(e, "left-side");
     toggleClass(e.nextElementSibling, "left-side");
-    
-  }
-  else if (page == currentPage - 1) {
-    currentPage-=2;
+  } else if (page == currentPage - 1) {
+    currentPage -= 2;
     toggleClass(e, "left-side");
     toggleClass(e.previousElementSibling, "left-side");
   }
-  
 }
+
+// Add touch event listeners
 document.addEventListener("touchstart", function (e) {
-  let target = e.target.closest(".page");
-  if (target) {
-    let pageNum = parseInt(target.getAttribute("onclick").match(/\d+/)[0]);
-    movePage(target, pageNum);
+  touchStartX = e.changedTouches[0].clientX;
+});
+
+document.addEventListener("touchend", function (e) {
+  touchEndX = e.changedTouches[0].clientX;
+  let diff = touchStartX - touchEndX;
+
+  if (Math.abs(diff) > 50) { // Minimum swipe distance
+    let pages = document.querySelectorAll(".page");
+    if (diff > 0 && currentPage < pages.length) {
+      movePage(pages[currentPage - 1], currentPage);
+    } else if (diff < 0 && currentPage > 1) {
+      movePage(pages[currentPage - 2], currentPage - 1);
+    }
   }
 });
